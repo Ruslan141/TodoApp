21684316cf2b9eeb23c93ebdeed42ad565e84374
Versoit <ruslan.itpro@gmail.com>
2023-06-11 20:29:14 +0300

    Added navigation component

---
+++ app/src/main/java/ru/versoit/todoapp/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.versoit.todoapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.versoit.todoapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
--- app/src/main/java/ru/versoit/todoapp/presentation/fragments/TasksFragment.kt
+++ app/src/main/java/ru/versoit/todoapp/presentation/fragments/TasksFragment.kt
@@ -5,19 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.versoit.todoapp.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
+    private lateinit var binding: FragmentTasksBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
